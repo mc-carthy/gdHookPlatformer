6ea6df079f9a5ac1d8f5d1beb1da72efb013f46a
Mick McCarthy <mick@mccarthy.tech>
2020-04-10 14:58:13 +0100

    Idle state finished

--- src/Player/States/Idle.gd
+++ src/Player/States/Idle.gd
@@ -1,2 +1,21 @@
 extends State
 
+onready var move:= get_parent()
+
+func unhandled_input(event: InputEvent) -> void:
+	move.unhandled_input(event)
+
+func physics_process(delta: float) -> void:
+	if owner.is_on_floor():
+		if move.get_move_direction().x != 0.0:
+			_state_machine.transition_to('Move/Run')
+	else:
+		_state_machine.transition_to('Move/Air')
+
+func enter(msg: Dictionary = {}) -> void:
+	move.enter(msg)
+	move.max_velocity = move.max_velocity_default
+	move.velocity = Vector2.ZERO
+
+func exit() -> void:
+	move.exit()
