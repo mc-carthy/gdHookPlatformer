80553b6ea72baf618d208cd42310427a8db11131
Mick McCarthy <mick@mccarthy.tech>
2020-04-10 20:54:55 +0100

    Create run state

--- src/Player/States/Idle.gd
+++ src/Player/States/Idle.gd
@@ -1,11 +1,11 @@
 extends State
 
-onready var move:= get_parent()
-
 func unhandled_input(event: InputEvent) -> void:
+	var move:= get_parent()
 	move.unhandled_input(event)
 
 func physics_process(delta: float) -> void:
+	var move:= get_parent()
 	if owner.is_on_floor():
 		if move.get_move_direction().x != 0.0:
 			_state_machine.transition_to('Move/Run')
@@ -13,9 +13,11 @@ func physics_process(delta: float) -> void:
 		_state_machine.transition_to('Move/Air')
 
 func enter(msg: Dictionary = {}) -> void:
+	var move:= get_parent()
 	move.enter(msg)
 	move.max_velocity = move.max_velocity_default
 	move.velocity = Vector2.ZERO
 
 func exit() -> void:
+	var move:= get_parent()
 	move.exit()
