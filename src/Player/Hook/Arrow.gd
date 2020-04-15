c378f7c177e76e859a7446bdf9482968dca64e2b
Mick McCarthy <mick@mccarthy.tech>
2020-04-15 21:00:23 +0100

    Create functioning hook

--- src/Player/Hook/Arrow.gd
+++ src/Player/Hook/Arrow.gd
@@ -1,2 +1,34 @@
 extends Node2D
 
+onready var head: Sprite = $Head
+onready var tail: Line2D = $Tail
+onready var tween: Tween = $Tween
+
+onready var length: float = tail.points[0].x setget set_length
+onready var start_length: float = head.position.x
+
+var hook_position: Vector2 = Vector2.ZERO setget set_hook_position
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		self.hook_position = get_global_mouse_position()
+
+func set_hook_position(value: Vector2) -> void:
+	hook_position = value
+	var to_target: Vector2 = hook_position - global_position
+	self.length = to_target.length()
+	rotation = to_target.angle()
+	tween.interpolate_property(
+		self,
+		'length',
+		length,
+		start_length,
+		0.25,
+		Tween.EASE_OUT
+	)
+	tween.start()
+
+func set_length(value: float) -> void:
+	length = value
+	tail.points[-1].x = length
+	head.position.x = tail.points[-1].x + tail.position.x
