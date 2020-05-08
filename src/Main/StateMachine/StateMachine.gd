9f5d57aa0c549f4cb3d75f87fa1b784c2a5d4b2d
Mick McCarthy <mick@mccarthy.tech>
2020-05-08 22:00:14 +0100

    Setup die state

--- src/Main/StateMachine/StateMachine.gd
+++ src/Main/StateMachine/StateMachine.gd
@@ -17,9 +17,6 @@ func _ready() -> void:
 	state.enter()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed('debug_spawn'):
-
-		transition_to('Spawn')
 	state.unhandled_input(event)
 
 func _physics_process(delta: float) -> void:
--- src/Player/Die.gd
+++ src/Player/Die.gd
@@ -1,23 +1,14 @@
 extends State
 
-var _start_position: Vector2 = Vector2.ZERO
-
-func _ready() -> void:
-	yield(owner, 'ready')
-	_start_position = owner.position
-
 func _on_skin_animation_finished(anim_name: String) -> void:
-	_state_machine.transition_to('Move/Idle')
+	_state_machine.transition_to('Spawn')
 
 func enter(msg: Dictionary = {}) -> void:
-	owner.hook.visible = false
-	owner.position = _start_position
 	owner.is_active = false
+	owner.hook.visible = false
 	#owner.camera_rig.is_active = false
-	owner.skin.play('spawn')
+	owner.skin.play('die')
 	owner.skin.connect('animation_finished', self, '_on_skin_animation_finished')
 
 func exit() -> void:
-	owner.is_active = true
-	owner.hook.visible = true
 	owner.skin.disconnect('animation_finished', self, '_on_skin_animation_finished')
