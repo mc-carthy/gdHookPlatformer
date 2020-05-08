3354bc67b7e60c9c1f55cccd5206c19fa9a6f944
Mick McCarthy <mick@mccarthy.tech>
2020-05-08 16:38:54 +0100

    Create player skin scene

---
+++ src/Player/Skin.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+signal animation_finished(name)
+
+onready var anim: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	anim.connect('animation_finished', self, '_on_animation_finished')
+
+func _on_animation_finished(anim_name: String) -> void:
+	emit_signal('animation_finished', anim_name)
+
+func play(anim_name: String) -> void:
+	assert (anim_name in anim.get_animation_list())
+	anim.play(anim_name)
