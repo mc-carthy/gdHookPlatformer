a76b75540cb1e09a7d1b0c6f1a9886bec4f78d04
Mick McCarthy <mick@mccarthy.tech>
2020-05-15 21:20:30 +0100

    Set up wall state

--- src/Player/States/Air.gd
+++ src/Player/States/Air.gd
@@ -43,6 +43,10 @@ func physics_process(delta: float) -> void:
 			_state_machine.transition_to('Move/Run')
 	elif owner.ledge_detector.is_against_ledge():
 		_state_machine.transition_to('Ledge', { move_state = move })
+	
+	if owner.is_on_wall():
+		var wall_normal: float = owner.get_slide_collision(0).normal.x
+		_state_machine.transition_to('Move/Wall', { normal = wall_normal, velocity = move.velocity})
 
 func enter(msg: Dictionary = {}) -> void:
 	var move:= get_parent()
